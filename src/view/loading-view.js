8d743dc5593ac3183523ed9682c3823f9521e3d8
Vladislav-Patrokeev <ihavealvxframe@gmail.com>
2025-06-13 00:36:39 +0500

    fix

--- src/view/loading-view.js
+++
@@ -1,7 +0,0 @@
-import AbstractView from '../framework/view/abstract-view';
-
-export default class LoadingView extends AbstractView {
-  get template() {
-    return '<p class="trip-events__msg">Loading...</p>';
-  }
-}
